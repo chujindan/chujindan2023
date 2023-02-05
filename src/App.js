383de65262f560f397dcb3dc578b6849d80f5eef
CJD <chujindan22@gmail.com>
2023-02-05 14:50:14 +0900

    2월 5일 수정

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         <AppRouter />
         <footer id="tail" style={{paddingTop: "1vh"}}>
           <div> &copy; 2023 5급 공채 총정모 추진단 </div>
-          <div style={{fontSize: "1vmin", display:"flex", justifyContent: "flex-start", width:"100%"}}> 
+          <div style={{display:"flex", justifyContent: "flex-start", width:"100%"}}> 
             <div style={{marginRight:"10px"}}>단장 정유나 📱010-7153-6227</div>
             <div>부단장 김현수 📱010-2385-0744</div>
           </div>
-          <div style={{fontSize: "0.5vmin"}}>홈페이지 관리자 김주호</div>
+          <div>홈페이지 관리자 김주호</div>
         </footer>
       </div>
     </>
