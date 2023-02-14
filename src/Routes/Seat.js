12d6bb96939860ea9b85c686b784ab9c214c5031
CJD <chujindan22@gmail.com>
2023-02-14 11:29:01 +0900

    2/14 1

--- src/Routes/Seat.js
+++ src/Routes/Seat.js
@@ -1,5 +1,5 @@
 import React from "react";
-import seatTable from '../seatTable.png';
+import seatTable from '../seatTable.jpg';
 import { Link } from "react-router-dom";
 import { AiOutlineInsertRowAbove } from "react-icons/ai";
 
